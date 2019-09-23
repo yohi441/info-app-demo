f566a195c5070fa9f7c5d71ce79c96d0cd0773ef
yohi <rikou92991@gmail.com>
2019-09-24 07:48:45 +0800

    update heroku config

--- information_kiosk/settings.py
+++ information_kiosk/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import decouple import config
+from decouple import config
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -141,5 +141,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-db_from_env = dj_database_url.config(conn_max_age==500)
-DATABASES['default'].update(db_from_env)
